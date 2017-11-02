abdea1b2f22b1c555932c9878996f3e4d2653ed5
reggiepdavison <github@rpdavison.ca>
2017-11-02 14:58:12 -0400

    Added units for some sensors. Modified manifest file to ensure that title remains constant when orientation changes.

--- app/src/main/java/intelliware/ca/sensible/DummySensorAdapter.java
+++ app/src/main/java/intelliware/ca/sensible/DummySensorAdapter.java
@@ -17,8 +17,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DummySensorAdapter implements SensorAdapter {
-    public static final String MICROSECONDS = " μs";
-    public static final String MILLIAMPS = " mA";
+    public static final String MICRO_SECONDS = " μs";
+    public static final String MICRO_TESLA = " μT";
+    public static final String MILLI_AMPS = " mA";
+    public static final String METRES_PER_SECOND_SQUARED = " m/s^2";
+    public static final String RADS_PER_SECOND = " rad/s";
+    public static final String SI_LUX = "SI lux";
+    public static final String MILLIBAR = " hPa";
+    public static final String CM = " cm";
+    public static final String DEGREES_CELCIUS = "°C";
     private View view = null;
     private Sensor sensor;
     private List<TextView> sensorValues = new ArrayList<>();
@@ -31,40 +38,78 @@ public class DummySensorAdapter implements SensorAdapter {
     @Override
     public View createView(LayoutInflater inflater, ViewGroup container, Resources resources) {
         view = inflater.inflate(R.layout.dummy_sensor, container, false);
-        TableLayout metadata = view.findViewById(R.id.metadata);
-        metadata.setPadding(30, 30, 30, 30);
-        metadata.addView(createTableRow("Vendor", sensor.getVendor()));
-        metadata.addView(createTableRow("Name", sensor.getName()));
-        metadata.addView(createTableRow("Type", sensor.getStringType() + " (" + sensor.getType() + ")"));
-        metadata.addView(createTableRow("ID", "" + sensor.getId()));
-        metadata.addView(createTableRow("FIFO Max", "" + sensor.getFifoMaxEventCount()));
-        metadata.addView(createTableRow("FIFO Reserved", "" + sensor.getFifoReservedEventCount()));
-        metadata.addView(createTableRow("Min Delay", "" + sensor.getMinDelay() + MICROSECONDS));
-        metadata.addView(createTableRow("Max Delay", "" + sensor.getMaxDelay() + MICROSECONDS));
-        metadata.addView(createTableRow("Max Range", "" + sensor.getMaximumRange()));
-        metadata.addView(createTableRow("Power", sensor.getPower() + MILLIAMPS));
-        metadata.addView(createTableRow("Reporting Mode", getReportingMode(sensor.getReportingMode())));
-        metadata.addView(createTableRow("Add'l Info Supported", Boolean.toString(sensor.isAdditionalInfoSupported())));
-        metadata.addView(createTableRow("Dynamic", Boolean.toString(sensor.isDynamicSensor())));
-        metadata.addView(createTableRow("Wake Up", Boolean.toString(sensor.isWakeUpSensor())));
+        createMetadata();
+        createValuesTable();
+        return view;
+    }
 
+    private void createValuesTable() {
         TableLayout table = view.findViewById(R.id.value_table);
-        for (int i = 0; i < 10; i++) {
+        for (int rowIndex = 0; rowIndex < 10; rowIndex++) {
             TableRow tableRow = new TableRow(view.getContext());
             tableRow.setVisibility(View.INVISIBLE);
-            TextView label = new TextView(view.getContext());
-            label.setText(i + ":");
-            label.setTypeface(Typeface.DEFAULT_BOLD);
-            label.setPadding(0, 0, 30, 0);
-            tableRow.addView(label);
+
+            tableRow.addView(createSensorValueLabel(sensor.getType(), rowIndex));
+
             TextView sensorValue = new TextView(view.getContext());
             sensorValue.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_END);
+
             sensorValues.add(sensorValue);
             tableRow.addView(sensorValue);
             table.addView(tableRow);
             tableRows.add(tableRow);
         }
-        return view;
+    }
+
+    @NonNull
+    private TextView createSensorValueLabel(int sensorType, int rowIndex) {
+        TextView label = new TextView(view.getContext());
+        label.setText(getRowLabel(sensorType, rowIndex) + ":");
+        label.setTypeface(Typeface.DEFAULT_BOLD);
+        label.setPadding(0, 0, 30, 0);
+        return label;
+    }
+
+    private String getRowLabel(int sensorType, int rowIndex) {
+        switch (sensorType) {
+            case Sensor.TYPE_ACCELEROMETER:
+            case Sensor.TYPE_ACCELEROMETER_UNCALIBRATED:
+            case Sensor.TYPE_GYROSCOPE:
+            case Sensor.TYPE_GYROSCOPE_UNCALIBRATED:
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                switch (rowIndex) {
+                    case 0:
+                        return "x";
+                    case 1:
+                        return "y";
+                    case 2:
+                        return "z";
+                    default:
+                        return "" + rowIndex;
+                }
+            default:
+                return "" + rowIndex;
+
+        }
+    }
+
+    private void createMetadata() {
+        TableLayout metadata = view.findViewById(R.id.metadata);
+        metadata.setPadding(30, 30, 30, 30);
+        metadata.addView(createMetadataTableRow("Vendor", sensor.getVendor()));
+        metadata.addView(createMetadataTableRow("Name", sensor.getName()));
+        metadata.addView(createMetadataTableRow("Type", sensor.getStringType() + " (" + sensor.getType() + ")"));
+        metadata.addView(createMetadataTableRow("ID", "" + sensor.getId()));
+        metadata.addView(createMetadataTableRow("FIFO Max", "" + sensor.getFifoMaxEventCount()));
+        metadata.addView(createMetadataTableRow("FIFO Reserved", "" + sensor.getFifoReservedEventCount()));
+        metadata.addView(createMetadataTableRow("Min Delay", "" + sensor.getMinDelay() + MICRO_SECONDS));
+        metadata.addView(createMetadataTableRow("Max Delay", "" + sensor.getMaxDelay() + MICRO_SECONDS));
+        metadata.addView(createMetadataTableRow("Max Range", "" + sensor.getMaximumRange()));
+        metadata.addView(createMetadataTableRow("Power", sensor.getPower() + MILLI_AMPS));
+        metadata.addView(createMetadataTableRow("Reporting Mode", getReportingMode(sensor.getReportingMode())));
+        metadata.addView(createMetadataTableRow("Add'l Info Supported", Boolean.toString(sensor.isAdditionalInfoSupported())));
+        metadata.addView(createMetadataTableRow("Dynamic", Boolean.toString(sensor.isDynamicSensor())));
+        metadata.addView(createMetadataTableRow("Wake Up", Boolean.toString(sensor.isWakeUpSensor())));
     }
 
     private String getReportingMode(int reportingMode) {
@@ -83,7 +128,7 @@ public class DummySensorAdapter implements SensorAdapter {
     }
 
     @NonNull
-    private TableRow createTableRow(String attribute, String textValue) {
+    private TableRow createMetadataTableRow(String attribute, String textValue) {
         TableRow row = new TableRow(view.getContext());
         TextView label = new TextView(view.getContext());
         label.setText(attribute + ": ");
@@ -108,10 +153,34 @@ public class DummySensorAdapter implements SensorAdapter {
             if (i < newValues.length) {
                 float newValue = newValues[i];
                 tableRow.setVisibility(View.VISIBLE);
-                sensorValueField.setText(new DecimalFormat("0.000").format(newValue));
+                sensorValueField.setText(new DecimalFormat("0.000").format(newValue) + getUnits(sensorEvent.sensor.getType(), i));
             } else {
                 tableRow.setVisibility(View.INVISIBLE);
             }
+        }
+    }
+
+    private String getUnits(int sensorType, int i) {
+        switch (sensorType) {
+            case Sensor.TYPE_ACCELEROMETER:
+            case Sensor.TYPE_GRAVITY:
+            case Sensor.TYPE_LINEAR_ACCELERATION:
+                return METRES_PER_SECOND_SQUARED;
+            case Sensor.TYPE_MAGNETIC_FIELD:
+            case Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED:
+                return MICRO_TESLA;
+            case Sensor.TYPE_GYROSCOPE:
+                return RADS_PER_SECOND;
+            case Sensor.TYPE_LIGHT:
+                return SI_LUX;
+            case Sensor.TYPE_PRESSURE:
+                return MILLIBAR;
+            case Sensor.TYPE_PROXIMITY:
+                return CM;
+            case Sensor.TYPE_AMBIENT_TEMPERATURE:
+                return DEGREES_CELCIUS;
+            default:
+                return "";
         }
     }
 
